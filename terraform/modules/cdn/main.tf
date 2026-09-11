4cb0aa99e99f464809204c5580ae94f390c886b4
diwakar <diwakar.valivarthi@gmail.com>
2026-09-11 15:57:38 +0530

    fix(terraform): split ALB listener rules to observe AWS limit of max 5 condition values per rule

--- terraform/modules/cdn/main.tf
+++ terraform/modules/cdn/main.tf
@@ -257,7 +257,24 @@ resource "aws_lb_listener_rule" "operations_api" {
         "/api/admin*",
         "/api/orders*",
         "/api/warehouses*",
-        "/api/quotations*",
+        "/api/quotations*"
+      ]
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "operations_api_catalog" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 25
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.operations.arn
+  }
+
+  condition {
+    path_pattern {
+      values = [
         "/api/products*",
         "/api/vendors*",
         "/api/categories*",
@@ -280,9 +297,7 @@ resource "aws_lb_listener_rule" "inventory_api" {
   condition {
     path_pattern {
       values = [
-        "/api/inventory*",
-        "/api/products*",
-        "/api/categories*"
+        "/api/inventory*"
       ]
     }
   }
