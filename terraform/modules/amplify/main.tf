53dcfd73ac029193a8c0a896003dfc918605ce67
DiwakarRajuValivarthi <115709702+valivarthi007@users.noreply.github.com>
2026-09-14 06:24:01 +0530

    fix: correct invalid regex syntax in amplify custom rules

--- terraform/modules/amplify/main.tf
+++ terraform/modules/amplify/main.tf
@@ -37,7 +37,7 @@ resource "aws_amplify_app" "sunotal" {
   }
 
   custom_rule {
-    source = "</^[^.]+$|\\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|woff2|ttf|map|json)$)([^.]+$)/>"
+    source = "^[^.]+$|\\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|woff2|ttf|map|json)$)([^.]+$)"
     target = "/index.html"
     status = "200"
   }
